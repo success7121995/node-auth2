d75430a8ef1282dd3f29352a1192a39320baf9ef
Stanford Tse <success7121995@gmail.com>
2023-06-26 21:35:40 +0800

    asdasd

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+const userRoutes = require('./routes/user-routes');
+
+// init app
+const app = express();
+
+// middlewares
+app.use(express.static('public'));
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
+// view engine
+app.set('view engine', 'ejs');
+
+// routes
+app.use('/user', userRoutes);
+
+//connect to db
+const port = process.env.PORT;
+const db_url = process.env.DB_URL;
+const connectdb = async () => {
+    try {
+        await mongoose.connect(db_url, {
+            useUnifiedTopology: true
+        });
+        app.listen(port, () => {
+            console.log(`listening on port ${port}`);
+        })
+    } catch (err) {
+        console.log(err);
+    };
+};
+connectdb();
+
+// home page
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+// 404 not found
+app.use((req, res) => {
+    res.render('404')
+})
---
+++ routes/user-routes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+// init router
+const router = express.Router();
+
+// routes
+// go to login page
+
+
+
+
+
+// exports
+module.exports = router;
