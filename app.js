848375b1745713dec4aaf69916e47dd10fd9cf27
Stanford Tse <success7121995@gmail.com>
2023-06-27 23:11:31 +0800

    WEFWEF

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
-// app.use(cookieParser());
+app.use(cookieParser());
 
 // view engine
 app.set('view engine', 'ejs');
