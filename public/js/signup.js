e9f9eecb304ec3b4d171c90995867fedf9c2f8a7
success7121995 <113827921+success7121995@users.noreply.github.com>
2023-07-06 07:04:45 +0000

    signup frontend function is not completed

--- public/js/signup.js
+++ public/js/signup.js
@@ -9,37 +9,22 @@ form.addEventListener('submit', async (e) => {
     const password = form.password.value;
     const password1 = form.password1.value;
 
-    // err messages
+    // error messages
     const usernameErr = document.querySelector('.username.error');
     const emailErr = document.querySelector('.email.error');
     const passwordErr = document.querySelector('.password.error');
     const password1Err = document.querySelector('.password1.error');
 
-    // reset message
+    // reset
     usernameErr.textContent = '';
     emailErr.textContent = '';
     passwordErr.textContent = '';
     password1Err.textContent = '';
 
+    // compare passwords, if matched, send data to server
     if (password !== password1) {
-        password1Err.textContent = 'Password is not match.';
-    } else {
-        try {            
-            const res = await fetch('/users/signup', {
-                method: 'POST',
-                body: JSON.stringify({ username, email, password }),
-                headers: { 'Content-Type': 'application/json'}
-            });
-        
-            const data = await res.json();
-            if(data.errors) {
-                usernameErr.textContent = data.errors.username;
-                emailErr.textContent = data.errors.email;
-                passwordErr.textContent = data.errors.password;
-            }
-            if (data.user) {
-                location.assign('/');
-            };
+        try {
+            
         } catch (err) {
             console.log(err);
         };
