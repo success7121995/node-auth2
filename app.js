1abad1c17d36775d58aa444d9ed741332d02ba1c
success7121995 <113827921+success7121995@users.noreply.github.com>
2023-07-06 02:34:45 +0000

    created 404 and home page

--- app.js
+++ app.js
@@ -12,5 +12,39 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(session({
     name: 'sid',
-    secret
-}))
+    secret: process.env.SESSION_SECRET,
+    saveUninitialized: false,
+    resave: true,
+    cookie: {
+        httpOnly: true,
+        secure: true,
+        maxAge: 1000 * 24 * 60 * 60
+    },
+}));
+
+// view engine
+app.set('view engine', 'ejs');
+
+// connect to db
+const connectDb = async () => {    
+    try {
+        await mongoose.connect(process.env.DB_URL, { useUnifiedTopology: true });
+        app.listen(process.env.PORT, () => {
+            console.log(`listening on port ${process.env.PORT}`);
+        })
+    } catch (err) {
+        console.log(err);
+    };
+};
+connectDb();
+
+// route
+// go to home page
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+// 404
+app.use((req, res) => {
+    res.status(404).render('404')
+});
